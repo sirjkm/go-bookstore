faee9a57495259e3e8fa6bac4e348e22ff6f3234
Jason Manning <jasonkylemanning@gmail.com>
2022-05-06 22:57:57 -0400

    add main.go file

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"go-bookstore/pkg/routes"
 	"log"
 	"net/http"
+
 	"github.com/gorilla/mux"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/jasonmanning/go-bookstore/pkg/routes"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
+func main() {
+	r:= mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
