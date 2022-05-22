bb3c5c4abbee2269f9f345b5b3595579e14dee65
Jason Manning <jasonkylemanning@gmail.com>
2022-05-21 22:04:00 -0400

    udpate

--- pkg/controllers/controller.go
+++ pkg/controllers/controller.go
@@ -25,7 +25,7 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 	bookId := vars["bookId"]
 	ID, err := strconv.ParseInt(bookId, 0, 0)
 	if err != nil {
-		fmt.Println("error while parsing")
+		fmt.Println("There was an error while parsing")
 	}
 	bookDetails, _ := models.GetBookById(ID)
 	res, _ := json.Marshal(bookDetails)
@@ -48,7 +48,7 @@ func DeleteBook(w http.ResponseWriter, r *http.Request) {
 	bookId := vars["bookId"]
 	ID, err := strconv.ParseInt(bookId, 0, 0)
 	if err != nil {
-		fmt.Println("error while parsing")
+		fmt.Println("There was an error while parsing")
 	}
 	book := models.DeleteBook(ID)
 	res, _ := json.Marshal(book)
@@ -64,7 +64,7 @@ func UpdateBook(w http.ResponseWriter, r *http.Request) {
 	bookId := vars["bookId"]
 	ID, err := strconv.ParseInt(bookId, 0, 0)
 	if err != nil {
-		fmt.Println("error while parsing")
+		fmt.Println("There was an error while parsing")
 	}
 	bookDetails, db := models.GetBookById(ID)
 	if updateBook.Name != "" {
